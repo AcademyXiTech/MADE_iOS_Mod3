55ebae7784c6a7e88f77cc180a38a991ebfbea12
Will Lumley <will@lumley.io>
2022-10-20 21:54:12 +1100

    Added new museums app.

---
+++ Museum/Museum/MuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumAppApp.swift
+//  Museum
+//
+//  Created by William Lumley on 6/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct MuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
